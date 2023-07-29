5ab4d40ab871db010582f9c0f04ed6ed85f4b34e
Isotop7 <hendrik@hr94.de>
2023-07-29 21:56:16 +0200

    update README add first option codes

--- include/MHET_Live_Barcode_Scanner.h
+++ include/MHET_Live_Barcode_Scanner.h
@@ -16,12 +16,54 @@ public:
   enum Code {                               
     FactoryReset,
     SaveUserDefaults,
-    RestoreUserDefaults
+    RestoreUserDefaults,
+    DeleteUserDefaults,
+    SetupModeOn,
+    SetupModeOff,
+    ReadModeTrigger,
+    ReadModeContinous,
+    ReadModeInduction,
+    ReadModeCommand,
+    InductionModeNightVisionOff,
+    InductionModeNightVisionOn,
+    ContinousModeSleepOff,
+    ContinousModeSleepOn,
+    MirrorHorizontalOff,
+    MirrorHorizontalOn,
+    MirrorVerticalOff,
+    MirrorVerticalOn,
+    AutomaticModeSameReadDelayOff,
+    AutomaticModeSameReadDelay100ms,
+    AutomaticModeSameReadDelay1s,
+    AutomaticModeSameReadDelay10s,
+    AutomaticModeSingleReadingTimeDelayOff,
+    AutomaticModeSingleReadingTimeDelay5s
   };                                
   std::map<Code, std::string> CodeMap = {     
-    {FactoryReset,        "~MA5F01B2C."},
-    {SaveUserDefaults,    "~MA5F0506A."},
-    {RestoreUserDefaults, "~MA5F08F37."}
+    {FactoryReset,                                "~MA5F01B2C."},
+    {SaveUserDefaults,                            "~MA5F0506A."},
+    {RestoreUserDefaults,                         "~MA5F08F37."},
+    {DeleteUserDefaults,                          "~MA5F0D201."},
+    {SetupModeOn,                                 "~M00910001."},
+    {SetupModeOff,                                "~M00910000."},
+    {ReadModeTrigger,                             "~M00210000."},
+    {ReadModeContinous,                           "~M00210001."},
+    {ReadModeCommand,                             "~M00210003."},
+    {ReadModeInduction,                           "~M00210002."},
+    {InductionModeNightVisionOff,                 "~M00260000."},
+    {InductionModeNightVisionOn,                  "~M00260001."},
+    {ContinousModeSleepOff,                       "~M00220000."},
+    {ContinousModeSleepOn,                        "~M00220001."},
+    {MirrorHorizontalOff,                         "~M00240000."},
+    {MirrorHorizontalOn,                          "~M00240001."},
+    {MirrorVerticalOff,                           "~M00250000."},
+    {MirrorVerticalOn,                            "~M00250001."},
+    {AutomaticModeSameReadDelayOff,               "~M00B00000."},
+    {AutomaticModeSameReadDelay100ms,             "~M00B00001."}, // 1    x 100ms as hex
+    {AutomaticModeSameReadDelay1s,                "~M00B0000A."}, // 10   x 100ms as hex
+    {AutomaticModeSameReadDelay10s,               "~M00B00064."}, // 100  x 100ms as hex
+    {AutomaticModeSingleReadingTimeDelayOff,      "~M00B10000."}, // 0     x 100ms as hex
+    {AutomaticModeSingleReadingTimeDelay5s,       "~M00B10032."}, // 50    x 100ms as hex
   };
 
   void begin(Stream &serial);
