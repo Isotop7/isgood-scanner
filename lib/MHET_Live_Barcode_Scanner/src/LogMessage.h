befda55a1cfe4ce7c06820d64cf568d1af65a72f
Isotop7 <hendrik@hr94.de>
2023-08-18 21:20:27 +0200

    add wrapper class for log message

---
+++ lib/MHET_Live_Barcode_Scanner/src/LogMessage.h
@@ -0,0 +1,29 @@
+#ifndef LOGMESSAGE_H
+#define LOGMESSAGE_H
+
+#include <Arduino.h>
+
+class LogMessage 
+{
+    public:
+        LogMessage(String component, String event, String message) : _component(component), _event(event), _message(message) {};
+
+        const String getComponent() const
+        {
+            return _component;
+        }
+        const String getEvent() const
+        {
+            return _event;
+        }
+        const String getMessage() const
+        {
+            return _message;
+        }
+    private:
+        String _component;
+        String _event;
+        String _message;
+};
+
+#endif // LOGMESSAGE_H
